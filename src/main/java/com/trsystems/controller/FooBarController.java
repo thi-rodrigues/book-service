fa3d03650cc895a3f367d02dce1d11c12fd4634c
Thiago Rodrigues <thiagorodrigues1203@outlook.com>
2026-04-26 23:11:54 -0300

    107. 1003 Microsserviços Robustos com Spring Cloud, Spring Boot Retry e Resilience4j

---
+++ src/main/java/com/trsystems/controller/FooBarController.java
@@ -0,0 +1,29 @@
+package com.trsystems.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
+
+@RestController
+@RequestMapping("book-service")
+public class FooBarController {
+	
+	private Logger logger = LoggerFactory.getLogger(FooBarController.class);
+	
+	@GetMapping("/foo-bar")
+//	@Retry(name = "default")
+	@Retry(name = "foo-bar")
+	public String fooBar() {
+		logger.info("Request to foo-bar is received");
+		var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
+//		return "Foo Bar!!!";
+		return response.getBody();
+	}
+	
+
+}
