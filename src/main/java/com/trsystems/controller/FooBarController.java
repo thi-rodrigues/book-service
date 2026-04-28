3d5ed13deba918fde045183e9aa1d323d7b4b1dd
Thiago Rodrigues <thiagorodrigues1203@outlook.com>
2026-04-27 21:22:03 -0300

    110. 1006 Conhecendo as Features de Rate Limit e BulkHead no Resilience4j

--- src/main/java/com/trsystems/controller/FooBarController.java
+++ src/main/java/com/trsystems/controller/FooBarController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 
 @RestController
 @RequestMapping("book-service")
@@ -17,15 +19,29 @@ public class FooBarController {
 	
 	int count = 0;
 	
-	@GetMapping("/foo-bar")
+//	@GetMapping("/foo-bar")
 //	@Retry(name = "default")
-//	@Retry(name = "foo-bar", fallbackMethod = "fallbackMethod") // Caso der falha mesmo com o retyr, chama o método no fallbackMethod
-	@CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod") // Caso der falha mesmo com o retyr, chama o método no fallbackMethod
+//	@Retry(name = "foo-bar", fallbackMethod = "fallbackMethod") // Caso der falha mesmo com o retry, chama o método no fallbackMethod
+//	@CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod") // Caso der falha mesmo com o CircuitBreaker, chama o método no fallbackMethod
+//	public String fooBar() {
+//		logger.info("Request to foo-bar is received: {}", ++count);
+//		var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
+////		return "Foo Bar!!!";
+//		return response.getBody();
+//	}
+	
+//	@GetMapping("/foo-bar")
+//	@RateLimiter(name = "default")
+//	public String fooBar() {
+//		logger.info("Request to foo-bar is received: {}", ++count);
+//		return "Foo Bar!!!";
+//	}
+	
+	@GetMapping("/foo-bar")
+	@Bulkhead(name = "default")
 	public String fooBar() {
 		logger.info("Request to foo-bar is received: {}", ++count);
-		var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
-//		return "Foo Bar!!!";
-		return response.getBody();
+		return "Foo Bar!!!";
 	}
 	
 	public String fallbackMethod(Exception e) {
